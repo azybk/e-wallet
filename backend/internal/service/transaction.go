21d88746654d782b7b7ed0656031958faf9a7c62
Azy BK <kumbaraazy@gmail.com>
2024-07-30 21:54:23 +0700

    commit

--- backend/internal/service/transaction.go
+++ backend/internal/service/transaction.go
@@ -24,6 +24,29 @@ func NewTransaction(accountRepository domain.AccountRepository,
 }
 
 func (t transactionService) TransferInquiry(ctx context.Context, req dto.TransferInquiryReq) (dto.TransferInquiryRes, error) {
+	user := ctx.Value("x-user").(dto.UserData)
+
+	myAccount, err := t.accountRepository.FindByUserId(ctx, user.ID)
+	if err != nil {
+		return dto.TransferInquiryRes{}, err
+	}
+
+	if myAccount == (domain.Account{}) {
+		return dto.TransferInquiryRes{}, domain.ErrAccountNotFound
+	}
+
+	dofAccount, err := t.accountRepository.FindByAccountNumber(ctx, req.AccountNumber)
+	if err != nil {
+		return dto.TransferInquiryRes{}, err
+	}
+
+	if dofAccount == (domain.Account{}) {
+		return dto.TransferInquiryRes{}, domain.ErrAccountNotFound
+	}
+
+	if myAccount.Balance < req.Amount {
+		return dto.TransferInquiryRes{}, domain.ErrInsuficientBlanace
+	}
 
 }
 
