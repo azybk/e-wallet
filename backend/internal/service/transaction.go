2b4f3ad90c5ccea4950555c37d0a5de2e638d303
Azy BK <kumbaraazy@gmail.com>
2024-08-15 22:35:08 +0700

    commit

--- backend/internal/service/transaction.go
+++ backend/internal/service/transaction.go
@@ -4,21 +4,24 @@ import (
 	"context"
 	"e_wallet/backend/domain"
 	"e_wallet/backend/dto"
+	"e_wallet/backend/internal/util"
+	"encoding/json"
+	"time"
 )
 
 type transactionService struct {
 	accountRepository domain.AccountRepository
-	transacionRepository domain.TransactionRepository
+	transactionRepository domain.TransactionRepository
 	cacheRepository domain.CacheRepository
 }
 
 func NewTransaction(accountRepository domain.AccountRepository,
-	transacionRepository domain.TransactionRepository,
+	transactionRepository domain.TransactionRepository,
 	cacheRepository domain.CacheRepository) domain.TransactionService {
 
 	return &transactionService {
 		accountRepository: accountRepository,
-		transacionRepository: transacionRepository,
+		transactionRepository: transactionRepository,
 		cacheRepository: cacheRepository,
 	}
 }
@@ -48,8 +51,77 @@ func (t transactionService) TransferInquiry(ctx context.Context, req dto.Transfe
 		return dto.TransferInquiryRes{}, domain.ErrInsuficientBlanace
 	}
 
+	inquiryKey := util.GenerateRandomNumber(32)
+	jsonData, _ := json.Marshal(req)
+
+	_ = t.cacheRepository.Set(inquiryKey, jsonData)
+
+	return dto.TransferInquiryRes{
+		InquiryKey: inquiryKey,
+	}, nil
 }
 
 func (t transactionService) TransferExecute(ctx context.Context, req dto.TransferExecuteReq) error {
+	val, err := t.cacheRepository.Get(req.InquiryKey)
+	if err != nil {
+		return domain.ErrInquiryNotFound
+	}
 
+	var reqInq dto.TransferInquiryReq
+	_ = json.Unmarshal(val, &reqInq)
+	if reqInq == (dto.TransferInquiryReq{}) {
+		return domain.ErrInquiryNotFound
+	}
+
+	user := ctx.Value("x-user").(dto.UserData)
+	myAccount, err := t.accountRepository.FindByUserId(ctx, user.ID)
+	if err != nil {
+		return err
+	}
+
+	dofAccount, err := t.accountRepository.FindByAccountNumber(ctx, reqInq.AccountNumber)
+	if err != nil {
+		return err
+	}
+
+	debitTransaction := domain.Transaction {
+		AccountId: myAccount.ID,
+		SofNumber: myAccount.AccountNumber,
+		DofNumber: dofAccount.AccountNumber,
+		TransactionType: "D",
+		Amount: reqInq.Amount,
+		TransactionDatetime: time.Now(),
+	}
+
+	err = t.transactionRepository.Insert(ctx, &debitTransaction)
+	if err != nil {
+		return err
+	}
+
+	creditTransaction := domain.Transaction{
+		AccountId: dofAccount.ID,
+		SofNumber: myAccount.AccountNumber,
+		DofNumber: dofAccount.AccountNumber,
+		TransactionType: "C",
+		Amount: reqInq.Amount,
+		TransactionDatetime: time.Now(),
+	}
+	err = t.transactionRepository.Insert(ctx, &creditTransaction)
+	if err != nil {
+		return err
+	}
+
+	myAccount.Balance -= reqInq.Amount
+	err = t.accountRepository.Update(ctx, &myAccount)
+	if err != nil {
+		return err
+	}
+
+	dofAccount.Balance += reqInq.Amount
+	err = t.accountRepository.Update(ctx, &dofAccount)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
